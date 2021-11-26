48a8b02ec3f204e7ac2806266fcc3ee14dbb5dd9
KevinLsilva <kevinleasil2002@gmail.com>
2021-11-26 16:27:26 -0300

    Primeiro Commit

---
+++ Comparativo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Comparativo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.WriteLine("Informe um número inteiro: ");
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Par");
+            }
+            else
+            {
+                Console.WriteLine("Impar!");
+            }
+        }
+    }
+}
